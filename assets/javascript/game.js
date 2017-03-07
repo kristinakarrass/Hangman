22e134a428472ce664a49947a565244ae94c9fd8
Kristina Karrass <kristinakarrass@gmail.com>
2017-03-07 18:25:36 -0500

    Tuesday

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,9 @@
-	// assign variables
 
-	var word = ["elephant", "beluga whale", "hippopotamus","grey wolf", "tiger", "kookaburra", "lion", "raccoon", "penguin", "sea lion"];
-	var audio = ["assets/audio/elephant.mp3", "assets/audio/beluga.mp3", "assets/audio/hippo.mp3", "assets/audio/greywolf.mp3", "assets/audio/tiger.mp3", "assets/audio/kookaburra.mp3", "assets/audio/lion.mp3", "assets/audio/raccoon.mp3", "assets/audio/penguin.mp3", "assets/audio/sealion.mp3"]
-	var imgSrc = ["assets/images/elephant.jpg", "assets/images/beluga.jpg", "assets/images/hippo.jpg", "assets/images/greywolf.jpg", "assets/images/tiger.jpg", "assets/images/kookaburra.jpg", "assets/images/lion.jpg", "assets/images/raccoon.jpg", "assets/images/penguin.jpg", "assets/images/sealion.jpg"];
-	var link = ["https://en.wikipedia.org/wiki/Elephant", "https://en.wikipedia.org/wiki/Beluga_whale", "https://en.wikipedia.org/wiki/Hippopotamus", "https://en.wikipedia.org/wiki/Gray_wolf", "https://en.wikipedia.org/wiki/Tiger", "https://en.wikipedia.org/wiki/Kookaburra", "https://en.wikipedia.org/wiki/Lion", "https://en.wikipedia.org/wiki/Raccoon", "https://en.wikipedia.org/wiki/Penguin", "https://en.wikipedia.org/wiki/Sea_lion"];
-	var arrLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	var arrLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	var word = ["GIANT PANDA", "ELEPHANT", "BELUGA WHALE", "HIPPOPOTAMUS","GREY WOLF", "TIGER", "KOOKABURRA", "LION", "RACCOON", "PENGUIN", "SEA LION"];
+	var audio = ["assets/audio/panda.mp3", "assets/audio/elephant.mp3", "assets/audio/beluga.mp3", "assets/audio/hippo.mp3", "assets/audio/greywolf.mp3", "assets/audio/tiger.mp3", "assets/audio/kookaburra.mp3", "assets/audio/lion.mp3", "assets/audio/raccoon.mp3", "assets/audio/penguin.mp3", "assets/audio/sealion.mp3"]
+	var imgSrc = ["assets/images/panda.jpg", "assets/images/elephant.jpg", "assets/images/beluga.jpg", "assets/images/hippo.jpg", "assets/images/greywolf.jpg", "assets/images/tiger.jpg", "assets/images/kookaburra.jpg", "assets/images/lion.jpg", "assets/images/raccoon.jpg", "assets/images/penguin.jpg", "assets/images/sealion.jpg"];
+	var link = ["https://en.wikipedia.org/wiki/Giant_panda", "https://en.wikipedia.org/wiki/Elephant", "https://en.wikipedia.org/wiki/Beluga_whale", "https://en.wikipedia.org/wiki/Hippopotamus", "https://en.wikipedia.org/wiki/Gray_wolf", "https://en.wikipedia.org/wiki/Tiger", "https://en.wikipedia.org/wiki/Kookaburra", "https://en.wikipedia.org/wiki/Lion", "https://en.wikipedia.org/wiki/Raccoon", "https://en.wikipedia.org/wiki/Penguin", "https://en.wikipedia.org/wiki/Sea_lion"];
 	var guesses = 10;
 	var randomWord, userInput;
 	var secretWord = [];
@@ -14,19 +13,6 @@
 	var checkWord = [];
 	var wins = 0;
 	var losses = 0;
-	// imgArr[0].src = "../images/elephant.jpg";
-	// imgArr[1].src = "../images/beluga.jpg";
-	// imgArr[2].src = "../images/hippo.jpg";
-	// imgArr[3].src = "../images/greywolf.jpg";	
-	// imgArr[4].src = "../images/tiger.jpg";
-	// imgArr[5].src = "../images/kookaburra.jpg";
-	// imgArr[6].src = "../images/lion.jpg";
-	// imgArr[7].src = "../images/raccoon.jpg";
-	// imgArr[8].src = "../images/penguin.jpg";
-	// imgArr[9].src = "../images/sealion.jpg";
-
-// start the game
-
 
 
 	window.onload = function () {
@@ -35,20 +21,24 @@
 				animalSound.play();
 		}
 
+		function pauseAudio () {
+			animalSound.pause();
+		}
+
 		document.getElementById("lives").innerHTML = guesses;
 		document.getElementById("losses").innerHTML = losses;
 		document.getElementById("timesWon").innerHTML = wins;	
 
 		document.getElementById("secretWord").onclick = function restartGame() {
 
-				var guesses = 10;
-
+				guesses = 10;
 				secretWord.length = 0; //empties word arrays for new game
 				wordArray.length = 0;
-
 				guessedLettersDisplay.length = 0; //empties guessed letters array for new game
 				guessedLetters.length = 0;
-				
+				pauseAudio();
+				document.getElementById("animal").src = "assets/images/questionmark.jpg";
+				document.getElementById("link").href = "#";
 				document.getElementById("usedLetters").innerHTML = guessedLettersDisplay;
 				document.getElementById("lives").innerHTML = guesses;
 
@@ -63,16 +53,13 @@
 				for (var i = 0; i < randomWord.length; i++) {
 
 					if (arrLetter.indexOf(randomWord.charAt(i)) !== -1) {
-						console.log(randomWord.charAt(i))
 						wordArray.push(" " + randomWord.charAt(i) + " "); 	//pushes letters with spaces into array to exchange with placeholder at right guess
-						checkWord.push(randomWord.charAt(i));				//pushes letters into array to compare with userGuess
 						secretWord.push(" __ ");							//pushes placeholder into array for display
 					}
 
 					else {
 						wordArray.push('\u00A0\u00A0\u00A0');				//pushes space into array	
 						secretWord.push('\u00A0\u00A0\u00A0');				//pushes spaces into array for display
-						checkWord.push(randomWord.charAt(i));				//pushes space into array
 					}
 				} //closes for loop
 
@@ -83,7 +70,7 @@
 
 			document.onkeyup = function() {
 
-				var userGuess = event.key = String.fromCharCode(event.keyCode).toLowerCase();
+				var userGuess = event.key = String.fromCharCode(event.keyCode).toUpperCase();
 				console.log(userGuess);
 
 
@@ -92,27 +79,12 @@
 
 
 					if (guessedLetters.indexOf(userGuess) === -1) {				//checks if user has guessed the letter before
-				
+				console.log(guessedLetters, userGuess);
 							guessedLettersDisplay.push(" " + userGuess + " ");	 //pushes guessed Letter into Array to display
 							guessedLetters.push(userGuess);
 
-								if (guesses === 0) {
-									losses++;
-									document.getElementById("losses").innerHTML = losses;
-									restartGame();
-								}
 
-								else if (secretWord.toString() === wordArray.toString()) {
-									wins++;
-									document.getElementById("animal").src = image;
-									document.getElementById("animalSound").src = animalSound;
-									document.getElementById("link").innerHTML = webAddress;
-									playAudio(animalSound);
-									document.getElementById("timesWon").innerHTML = wins;
-									restartGame();
-								}
-
-								else if (checkWord.indexOf(userGuess) !== -1) {
+								if (randomWord.indexOf(userGuess) !== -1) {
 
 										for (var i = 0; i < randomWord.length; i++){
 
@@ -125,28 +97,43 @@
 
 								} // closes else if statement
 
+
+
+
 								else {
 									guesses--;
 									document.getElementById("lives").innerHTML = guesses;
 									// guessedLetters.push(userGuess);
 									document.getElementById("usedLetters").innerHTML = guessedLettersDisplay.join("");						
 								}
-				// } //closes first for loop
-					}
+	
+								if (secretWord.toString() === wordArray.toString()) {
+									wins++;
+									document.getElementById("animal").src = image;
+									document.getElementById("animalSound").src = animalSound;
+									document.getElementById("link").href = webAddress;
+									playAudio(animalSound);
+									document.getElementById("timesWon").innerHTML = wins;
+									// restartGame();
+								}		
+								else if (guesses === 0) { // change this to before the loop starts!!!
+									losses++;
+									document.getElementById("losses").innerHTML = losses;
+									// restartGame();
+								}			
+							}
 
 					else {
 						alert("You have already used this letter!");
 					}
 				
-				document.getElementById('secretWord').innerHTML = secretWord.join("");
+				document.getElementById('secretWord').innerHTML = secretWord.join("");  // updates secretWord with guesses letters
 	} //closes if statement that checks for valid user input
 					
 	else {
 		alert("Please choose a letter.");
 	}
-			} // closes onkeyup function
-			
-		} //closes function gameStart
 
-
-	}   // closes onload function
+   } // closes onkeyup function
+  } //closes function gameStart
+}   // closes onload function
